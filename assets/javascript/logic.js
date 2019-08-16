66f12041d181fe9e9e5a37c7833e919884a99cfd
arsanson <arsanson@mindspring.com>
2019-08-16 13:56:48 -0400

    :q!

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,29 +1,53 @@
-    //Javascript for smooth-scroll 
-    $('a[href*="#"]')
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function (event) {
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function () {
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) {
-                            return false;
-                        } else {
-                            $target.attr('tabindex', '-1');
-                        };
-                    });
-                }
+//Javascript for smooth-scroll 
+$('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1');
+                    };
+                });
             }
-        });
+        }
+    });
+
+// logic for mobile Nav-bar
+(function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('#' + burger.dataset.target);
+
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+    });
+})();
+
+// helps with the view height for mobile devices
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+//Creates Title of Each Level
+$(".nav-item").on("click", function(){
+
+})
